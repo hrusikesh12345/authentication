d6012611a3c7f4ddbc0cd31b5688236843e52911
hrusikesh padhy <hrusikesh@spikewell.com>
2025-02-13 12:53:05 +0530

    sx

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // Your backend server
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  
 });
